9922ec9f989c8b5208bac125e368c1021771c9da
aswanar <narottam.aswal@iss-stoxx.com>
2026-04-13 20:20:18 +0530

    feat. updated bloom filter

--- backend/src/main/java/com/hush/app/bloom/SimpleBloomFilter.java
+++ backend/src/main/java/com/hush/app/bloom/SimpleBloomFilter.java
@@ -1,23 +1,30 @@
 package com.hush.app.bloom;
 
-
+import io.micrometer.common.util.StringUtils;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SimpleBloomFilter {
-    private boolean[] switches = new boolean[10];
+    private static final int FILTER_SIZE = 8192;
+    private boolean[] switches = new boolean[FILTER_SIZE];
 
     private int getIndexA(String url) {
-        return url.length() % 10;
+        return Math.abs(url.hashCode()) % FILTER_SIZE; // Use hashCode for better distribution
     }
 
     private int getIndexB(String url) {
-        return (int) url.charAt(0) % 10;
+        int hash = 7;
+        for (int i = 0; i < url.length(); i++) {
+            hash = hash * 31 + url.charAt(i); // Custom hash function
+        }
+        return Math.abs(hash) % FILTER_SIZE;
     }
 
     public void add(String url) {
-        switches[getIndexA(url)] = true;
-        switches[getIndexB(url)] = true;
+        if(!StringUtils.isBlank(url)){
+            switches[getIndexA(url)] = true;
+            switches[getIndexB(url)] = true;
+        }
     }
 
     public boolean isMaybeTaken(String url) {

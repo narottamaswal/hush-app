1f544ab02741b4869acbc13d8d12f5b9c4fb6e39
aswanar <narottam.aswal@iss-stoxx.com>
2026-06-15 00:59:11 +0530

    feat. updated redis

--- backend/src/main/java/com/hush/app/config/security/SecurityConfig.java
+++ backend/src/main/java/com/hush/app/config/security/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                  .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+//                  .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
